89cf6d556b74a463bb081b699dce89f84e76a85c
chandakrohit8 <chandakrohit1@gmail.com>
2023-06-16 18:37:51 +0530

    Added Set_Matrix_Zeroes.cpp file

---
+++ Set_Matrix_Zeroes.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeros(vector<vector<int>> &matrix)
+{
+	//Efficent Approach Code 
+
+	int r=matrix.size();
+	int c=matrix[0].size();
+	vector<int> row(r,-1);
+	vector<int> col(c,-1);
+
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			if(matrix[i][j]==0)
+			{
+				row[i]=0;
+				col[j]=0;
+
+				//Mark the row and coloumn to be 0 
+			}
+		}
+	}
+
+	for(int i=0;i<r;i++)
+	{
+		for(int j=0;j<c;j++)
+		{
+			if(row[i]==0||col[j]==0)
+			{
+				matrix[i][j]=0;
+			}
+		}
+	}
+}
